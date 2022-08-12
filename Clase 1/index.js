d148cfed96457b01ba9cbb40a39f5e8355ebea93
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-08-11 21:31:36 -0500

    Clase 1

---
+++ Clase 1/index.js
@@ -0,0 +1,59 @@
+//comentario de linea
+
+/* 
+comentario
+de
+bloque
+*/
+
+//fomra de declaracion una variable no recomendada
+/* var edad;
+
+//fomra de declaracion una variable recomendada
+let nombre = "andres";
+let numero = 2;  */
+
+/* const EDAD = 20;
+
+EDAD = 25; */
+
+/* let numeroUno = 5;
+let numeroDos = 20;
+let suma = numeroUno + numeroDos;
+
+console.log("aca esta el resultado de la suma", suma); */
+
+/* let saludo = "Hola";
+let nombre = "Javier";
+let mensaje = saludo + " " + nombre;
+
+console.log(mensaje); */
+/* let numeroUno = 4;
+let numeroDos = "20";
+
+console.log(typeof(numeroUno));
+console.log(typeof(numeroDos)); */
+
+/* console.log(1 - 2);
+console.log(1 - 2); */
+
+/* let nombre = prompt("Ingrese su nombre");
+let mensaje = "Hola " + nombre;
+
+alert(mensaje); */
+
+/* let nombre = "andres";
+let edad = 50;
+
+let mensaje = `Hola ${nombre} tienes ${edad} de edad`;
+console.log(mensaje); */
+
+/* let edad = prompt("ingresa la edad");
+alert(edad); */
+
+let numeroUno = parseInt(prompt("Ingrese el primer numero"));
+let numeroDos = parseInt(prompt("Ingrese el segundo numero"));
+
+let suma = numeroUno + numeroDos;
+
+alert(suma);
