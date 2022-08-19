9a79cb6472b9fda740235cb032b1fedea7818ab9
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-08-19 16:52:08 -0500

    After1

---
+++ After 1/index.js
@@ -0,0 +1,41 @@
+let nombre = false;
+const EDAD = 30;
+
+// nombre = "andres";
+
+/* if(nombre === false){
+  console.log("nombre es false");
+}else if(nombre === "andres"){
+  let mensaje = "Hola andres";
+  console.log(mensaje)
+}else if(nombre === "Yeison"){
+  let mensaje = "Hola yeison";
+  console.log(mensaje)
+}else{
+  console.log("quién eres?");
+} */
+
+/* for(let index = 0; index < 4; index++){
+  console.log("Hola");
+} */
+
+/* let repetir = false;
+
+while(repetir){
+  console.log("entramos");
+}
+
+do{
+  console.log("entramos");
+}while(repetir) */
+
+let numero = parseInt(prompt("Ingrese un numero"));
+
+if(numero % 2 === 0){
+  alert("Es par");
+}else{
+  alert("Es impar");
+}
+
+let numero2 = 5;
+let numero1 = 7;
