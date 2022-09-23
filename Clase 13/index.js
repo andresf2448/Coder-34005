834297e43ccae458747daf9e4e93872207da3cf9
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-09-22 20:54:17 -0500

    Clase 13

--- Clase 13/index.js
+++ Clase 13/index.js
@@ -1,4 +1,5 @@
-let boton = document.getElementById("boton");
+//SWEETALERT
+// let boton = document.getElementById("boton");
 /* boton.addEventListener("click", () => {
   Swal.fire({
     title: "Error!",
@@ -82,3 +83,94 @@ let boton = document.getElementById("boton");
     }
   })
 }); */
+
+//TOASTIFY
+// let boton = document.getElementById("boton");
+
+/* boton.addEventListener("click", () => {
+  Toastify({
+    text: "Probando toast!",
+    duration: 5000
+  }).showToast();
+}) */
+
+/* boton.addEventListener("click", () => {
+  Toastify({
+    text: "Probando toast!",
+    duration: 3000,
+    style: {
+      background: "linear-gradient(to left, #00b09b, #96c92d)",
+    },
+  }).showToast();
+}); */
+
+/* boton.addEventListener("click", () => {
+  Toastify({
+    text: "Probando toast!",
+    duration: 3000,
+    className: 'notificacion'
+}).showToast();
+}); */
+
+/* boton.addEventListener("click", () => {
+  Toastify({
+    text: "Probando toast!",
+    duration: 3000,
+    onClick: () => {
+      Toastify({
+        text: "Clickeaste un Toast!",
+        duration: 1500,
+      }).showToast();
+    },
+  }).showToast();
+}); */
+
+/* boton.addEventListener("click", () => {
+  Toastify({
+    text: "Click aquí para ir a Coder!",
+    duration: 3000,
+    destination: "https://www.coderhouse.com",
+  }).showToast();
+}); */
+
+//LUXON
+// const DateTime = luxon.DateTime;
+/* const dt = DateTime.local(2022, 9, 22, 12, 45);
+console.log(dt.c); */
+
+// const hoy = DateTime.now();
+/* console.log(hoy.toString());
+console.log(hoy.year);
+console.log(hoy.month);
+console.log(hoy.day);
+console.log(hoy.second);
+console.log(hoy.weekday); */
+// console.log(hoy.zoneName);
+// console.log(hoy.daysInMonth);
+
+/* console.log(hoy.toLocaleString());
+console.log(hoy.toLocaleString(DateTime.DATE_FULL));
+console.log(hoy.toLocaleString(DateTime.TIME_SIMPLE));
+console.log(hoy.setLocale("es").toLocaleString(DateTime.DATE_FULL));
+console.log(hoy.setLocale("en").toLocaleString(DateTime.DATE_FULL)); */
+
+// const DateTime = luxon.DateTime;
+// const hoy = DateTime.now();
+
+// console.log(hoy.toLocaleString(DateTime.DATETIME_SHORT));
+
+// const suma = hoy.plus({hours: 1, minutes: 10});
+// console.log(suma.toLocaleString(DateTime.DATETIME_SHORT));
+
+// const resta = hoy.minus({month: 1, days: 10});
+// console.log(resta.toLocaleString(DateTime.DATETIME_SHORT));
+
+const DateTime = luxon.DateTime;
+const Interval = luxon.Interval;
+const hoy = DateTime.now();
+const navidad = DateTime.local(2022, 12, 25);
+
+const i = Interval.fromDateTimes(hoy, navidad);
+console.log(i.length("days"));
+console.log(i.length("hours"));
+console.log(i.length("minutes"));
